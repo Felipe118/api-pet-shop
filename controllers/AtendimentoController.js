c6c1d90659767c660f5f5e8c5b7e53d8853243ad
Felipe118 <lipebezerraa@gmail.com>
2022-03-01 16:19:29 -0300

    Salvando atendimentos

--- controllers/AtendimentoController.js
+++ controllers/AtendimentoController.js
@@ -1,4 +1,4 @@
-
+const Atendimento = require('../models/Atendimento')
 module.exports = class AtendimentoController{
 
     static atendimento(req,res){
@@ -6,9 +6,44 @@ module.exports = class AtendimentoController{
     }
 
     static async atendimentoPost(req,res){
-        const name = req.body.name
-        const age = req.body.age
-        console.log(name,age)
+       
+        const cliente = req.body.cliente
+        const pet = req.body.pet
+        const status = req.body.status
+        const servico = req.body.servico
+        const obs = req.body.obs
+
+
+        if(!cliente){
+            res.status(422).json({message: "O cliente é obrigatório"})
+            return
+        }
+        if(!status){
+            res.status(422).json({message: "O status é obrigatório"})
+            return
+        }
+        if(!servico){
+            res.status(422).json({message: "O serviço é obrigatório"})
+            return
+        }
+
+            
+        const atendimento = {
+            cliente:cliente,
+            pet:pet,
+            status:status,
+            servico:servico,
+            obs:obs
+        }
+
+        Atendimento.create(atendimento)
+        .then(()=> {
+            res.status(200).json({
+                message: "Atendimento cadastrado com sucesso!"
+            })  
+        })
+        .catch((erro)=> console.log(erro))
+        
 
         //return name
     }
